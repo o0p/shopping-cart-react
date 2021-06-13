acb746c8aa594a5bd5a6a53e94c42e03e16f19ba
Ruslan <rus@gmx.com>
2021-06-13 22:57:02 +0100

    Add action to reducer

--- src/reducer.js
+++ src/reducer.js
@@ -28,6 +28,13 @@ const reducer = (state, action) => {
       .filter((cartItem) => cartItem.amount !== 0);
     return { ...state, cart: tempCart };
   }
+  if (action.type === 'GET_TOTALS') {
+    const { total, amount } = state.cart.reduce(() => {}, {
+      total: 0,
+      amount: 0,
+    });
+    return { ...state, total, amount };
+  }
   return state;
 };
 export default reducer;
