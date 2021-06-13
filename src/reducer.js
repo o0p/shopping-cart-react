b8cb5a0e4ca9f6f33b8db8cb6ae78e6c8f07258c
Ruslan <rus@gmx.com>
2021-06-13 23:10:15 +0100

    Count total price and fix weird number bug

--- src/reducer.js
+++ src/reducer.js
@@ -29,9 +29,12 @@ const reducer = (state, action) => {
     return { ...state, cart: tempCart };
   }
   if (action.type === 'GET_TOTALS') {
-    const { total, amount } = state.cart.reduce(
+    let { total, amount } = state.cart.reduce(
       (cartTotal, cartItem) => {
         const { price, amount } = cartItem;
+        const itemTotal = price * amount;
+
+        cartTotal.total += itemTotal;
         cartTotal.amount += amount;
         return cartTotal;
       },
@@ -40,6 +43,7 @@ const reducer = (state, action) => {
         amount: 0,
       }
     );
+    total = parseFloat(total.toFixed(2));
     return { ...state, total, amount };
   }
   return state;
