aa2402389326d337d3e8eef29c80dbb57ab494aa
Ruslan <rus@gmx.com>
2021-06-14 22:36:58 +0100

    Add async loading

--- src/reducer.js
+++ src/reducer.js
@@ -49,5 +49,9 @@ const reducer = (state, action) => {
   if (action.type === 'LOADING') {
     return { ...state, loading: true };
   }
+  if (action.type === 'DISPLAY_ITEMS') {
+    return { ...state, cart: action.payload, loading: false };
+  }
+  return state;
 };
 export default reducer;
